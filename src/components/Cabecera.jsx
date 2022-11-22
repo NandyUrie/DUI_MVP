886215f1c33432a77763444dc42f5eef66760cf7
NandyUrie <93169156+NandyUrie@users.noreply.github.com>
2022-11-22 20:27:22 -0300

    intento final

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -6,7 +6,7 @@ export default class Cabecera extends Component{
     render(){
         return (
             <header className='App-header'>
-                <a href='http://localhost:5050/home'><img src={logo} className='App-logo' alt='logo' onClick='http://localhost:5050/home'/></a>
+                <a href='http://localhost:5050/'><img src={logo} className='App-logo' alt='logo' onClick='http://localhost:5050/'/></a>
             </header>
         )
     }
